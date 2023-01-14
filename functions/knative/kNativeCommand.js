09e7dd1c3fcb8733dfdc6c18092ded0358f8baf9
Tequilac <ptekielak@gmail.com>
2023-01-14 13:50:51 +0100

    Tests

--- functions/knative/kNativeCommand.js
+++ functions/knative/kNativeCommand.js
@@ -1,30 +1,34 @@
 // Runs a function in a KNative cluster
 
-const {exec} = require('child_process');
+// const {exec} = require('child_process');
 
 
-async function kNativeInvokeFunction(context, ins) {
-    console.log(`Invoking function ${context.name}`);
-    exec(`func invoke --data ${ins[0]}`, (err, stdout, stderr) => {
-        if (err) {
-            console.log(`Error while invoking function ${context.name}: ${stderr}`);
-        } else {
-            console.log(stdout);
-        }
-    });
-}
+// async function kNativeInvokeFunction(context, ins) {
+//     console.log(`Invoking function ${context.name}`);
+//     exec(`func invoke --data ${ins[0]}`, (err, stdout, stderr) => {
+//         if (err) {
+//             console.log(`Error while invoking function ${context.name}: ${stderr}`);
+//         } else {
+//             console.log(stdout);
+//         }
+//     });
+// }
 
 
 async function kNativeCommand(ins, outs, context, cb) {
-    console.log(`Deploying function ${context.name}`);
-    exec(`func deploy --path ${context.path} --repository ${context.repository}`, (err, stdout, stderr) => {
-        if (err) {
-            console.log(`Error while deploying function ${context.name}: ${stderr}`);
-        } else {
-            console.log(stdout);
-            kNativeInvokeFunction(context, ins);
-        }
-    });
+    console.log(ins);
+    console.log(outs);
+    console.log(context);
+    console.log(cb);
+    // console.log(`Deploying function ${context.name}`);
+    // exec(`func deploy --path ${context.path} --repository ${context.repository}`, (err, stdout, stderr) => {
+    //     if (err) {
+    //         console.log(`Error while deploying function ${context.name}: ${stderr}`);
+    //     } else {
+    //         console.log(stdout);
+    //         kNativeInvokeFunction(context, ins);
+    //     }
+    // });
 }
 
 exports.kNativeCommand = kNativeCommand;
