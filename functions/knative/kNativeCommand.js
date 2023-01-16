3663d4f184b833b97d4dca1ad149eac753f5bc02
Tequilac <ptekielak@gmail.com>
2023-01-16 18:36:43 +0100

    Tests

--- functions/knative/kNativeCommand.js
+++ functions/knative/kNativeCommand.js
@@ -19,66 +19,66 @@ spec:
 
 var interpolate = (tpl, args) => tpl.replace(/{(\w+)}/g, (_, v) => args[v]);
 
+async function kNativeCommand(ins, outs, context, cb) {
 
-function createData(ins) {
-    const dataString = `
+    function createData() {
+        const dataString = `
         - name: {key}
           value: "{value}"`;
-    amountParams = {
-        key: "DATA_NUM",
-        value: ins.dataUrl.data[0].length
-    }
-    let result = interpolate(dataString, amountParams);
-    for (let i = 0; i < ins.dataUrl.data[0].length; i++) {
-        params = {
-            key: `DATA${i}`,
-            value: ins.dataUrl.data[0][i]
+        amountParams = {
+            key: "DATA_NUM",
+            value: ins.dataUrl.data[0].length
         }
-        result += interpolate(dataString, params);
-    }
-    return result;
-}
-
-async function execute(spec, client, url) {
-    const response = await fetch(url);
-    const json = await response.json();
-    console.log(json);
-    await deleteService(spec, client);
-    return json;
-}
-
-async function getCondition(spec, client, name, url, outs, cb) {
-    response = await client.read({
-        apiVersion: "apps/v1",
-        kind: "Deployment",
-        metadata: {
-            name: `${name}-deployment`
+        let result = interpolate(dataString, amountParams);
+        for (let i = 0; i < ins.dataUrl.data[0].length; i++) {
+            dataParams = {
+                key: `DATA${i}`,
+                value: ins.dataUrl.data[0][i]
+            }
+            result += interpolate(dataString, dataParams);
         }
-    });
-    const condition = response.body.status.conditions[0].type;
-    console.log(`Current condition: ${condition}`);
-    if (condition !== "Available") {
-        setTimeout(() => getCondition(spec, client, name, url, outs, cb), 1000);
-    } else {
-        outs[0].data = await execute(spec, client, url);
-        cb(null, outs);
+        return result;
     }
-}
 
-async function deleteService(spec, client) {
-    console.log("Deleting...");
-    await client.delete(spec);
-    console.log("Service deleted");
-}
+    async function execute(spec, client, url) {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json);
+        await deleteService(spec, client);
+        return json;
+    }
 
-async function scheduleExecution(spec, client, outs, cb) {
-    let response = await client.read(spec);
-    const url = response.body.status.url;
-    console.log("Obtained service url: " + url);
-    setTimeout(() => getCondition(spec, client, response.body.status.latestCreatedRevisionName, url, outs, cb), 1000);
-}
+    async function getCondition(spec, client, name, url) {
+        response = await client.read({
+            apiVersion: "apps/v1",
+            kind: "Deployment",
+            metadata: {
+                name: `${name}-deployment`
+            }
+        });
+        const condition = response.body.status.conditions[0].type;
+        console.log(`Current condition: ${condition}`);
+        if (condition !== "Available") {
+            setTimeout(() => getCondition(spec, client, name, url), 1000);
+        } else {
+            outs[0].data = await execute(spec, client, url);
+            cb(null, outs);
+        }
+    }
 
-async function kNativeCommand(ins, outs, context, cb) {
+    async function deleteService(spec, client) {
+        console.log("Deleting...");
+        await client.delete(spec);
+        console.log("Service deleted");
+    }
+
+    async function scheduleExecution(spec, client) {
+        let response = await client.read(spec);
+        const url = response.body.status.url;
+        console.log("Obtained service url: " + url);
+        setTimeout(() => getCondition(spec, client, response.body.status.latestCreatedRevisionName, url), 1000);
+    }
+
     const kubeconfig = new k8s.KubeConfig();
     kubeconfig.loadFromDefault();
 
